6f1062a4baca4e7b61e580a3fa59959958547a57
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-05-25 12:04:02 +0300

    publish photo to Telegram channel.

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ if __name__ == '__main__':
     chat_id = "@cosmos_py"
     bot = telegram.Bot(token=token)
     bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+    photo = open('images/nasax_1..jpg', 'rb')
+    bot.send_photo(chat_id, photo)
