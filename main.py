ce9c74c4d61afddbb9334930364fd153b5006d30
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-05-25 11:28:30 +0300

    publish text to Telegram channel

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import telegram
+from dotenv import load_dotenv
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    token = os.getenv("TOKEN")
+    chat_id = "@cosmos_py"
+    bot = telegram.Bot(token=token)
+    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
