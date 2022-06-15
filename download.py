6b544c560595362d7c7afee0609e68013dca3a81
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-06-15 12:01:54 +0300

    Download function

---
+++ download.py
@@ -0,0 +1,7 @@
+import requests
+
+def download_img(url, file_path, payload=None):
+    response = requests.get(url, payload)
+    response.raise_for_status()
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
