4b4d14705c9b1ef1ea5bdbc17fe562bf5f89ba62
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-06-15 12:08:16 +0300

    Modified script

--- nasa.py
+++ nasa.py
@@ -1,23 +1,47 @@
 import os
+import json
 from pathlib import Path
+from urllib.parse import urlparse
 
 import requests
+from dotenv import load_dotenv
+
+from download import download_img
 
 
-file_path = "images_n"
-os.makedirs(file_path, exist_ok=True)
+def get_extension(urls):
+    for url in urls:
+        parse = urlparse(url)
+        extension = (os.path.splitext(parse.path))[1]
+    return extension
 
 
-url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-filename = "nasa.png"
-path = f"{file_path}/{filename}"
+def fetch_nasa_last_launch(response, directory):
+    urls = []
+    nasa_json = response.json()
+    search_key = 'url'
+    for nasa in nasa_json:
+        if search_key in nasa.keys():
+            urls.append(nasa['url'])
+    extension = get_extension(urls)
+    for url_number, url in enumerate(urls):
+        filename = f"nasax_{url_number}{extension}"
+        file_path = os.path.join(directory, filename)
+        download_img(url, file_path)
 
 
-def image_url(url, path):
-    response = requests.get(url)
+def main():
+    load_dotenv()
+    directory = "images"
+    os.makedirs(directory, exist_ok=True)
+    token = os.getenv("TOKEN_NASA")
+    count_link = 50
+    payload = {"count": count_link,
+               "api_key": token}
+    url = 'https://api.nasa.gov/planetary/apod'
+    response = requests.get(url, payload)
     response.raise_for_status()
-    with open(path, 'wb') as file:
-        file.write(response.content)
-    file.close()
-    print(url)
-image_url(url, path)
+    fetch_nasa_last_launch(response, directory)
+
+if __name__ == "__main__":
+    main()
