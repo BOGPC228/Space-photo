0c2d37587b45e2122dbc1ff67e738b0d4709ce3f
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-06-22 12:04:56 +0300

    Corrected names env

--- pyblish_image_to_telegram.py
+++ pyblish_image_to_telegram.py
@@ -4,18 +4,18 @@ from dotenv import load_dotenv
 from time import sleep
 
 
-def send_message(directory, chat_id ,time_day):
+def send_message(directory, chat_id , publication_delay_time):
     for root, dirs, files in os.walk(directory):
         for filee in files:
             file = filee
             photo = open(f"{directory}/{file}", 'rb')
             bot.send_photo(chat_id, photo)
-            sleep(time_day)
+            sleep(publication_delay_time)
 
 
-def un_flood(directory, chat_id ,time_day):
+def un_flood(directory, chat_id , publication_delay_time):
     while True:
-        send_message(directory, chat_id ,time_day)
+        send_message(directory, chat_id , publication_delay_time)
 
 
 if __name__ == '__main__':
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     directory = "images"
     token = os.getenv("TOKEN_BOT")
     chat_id = os.getenv("CHAT_ID_TG")
-    time_day = os.getenv("TIME")
-    time_day = int(time_day)
+    publication_delay_time = os.getenv("DELAY_TIME")
+    publication_delay_time = int(publication_delay_time)
     bot = telegram.Bot(token=token)
-    un_flood(directory, chat_id ,time_day)
+    un_flood(directory, chat_id , publication_delay_time)
