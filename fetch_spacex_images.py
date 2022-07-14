13a176ee04849855d56f201330b92114db8f77f3
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-07-14 12:27:21 +0300

    Fixed code

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -10,11 +10,11 @@ def fetch_spacex_last_launch(directory):
     url = "https://api.spacexdata.com/v3/launches"
     response = requests.get(url)
     response.raise_for_status()
-    start_all_spacex = response.json()
-    for start_one_spacex in start_all_spacex:
+    spacex_link = []
+    all_spacex_starts = response.json()
+    for start_one_spacex in all_spacex_starts:
         if start_one_spacex['links']['flickr_images']:
-            spacex_link = start_one_spacex['links']['flickr_images']
-            break
+            spacex_link+=start_one_spacex['links']['flickr_images']
     for url_number, url in enumerate(spacex_link):
         parse = urlparse(url)
         path, extension = os.path.splitext(parse.path)
