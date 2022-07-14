44e0d395f99b36b829b370190dd6cf0659f741b1
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-07-14 13:00:40 +0300

    Fixed code

--- publish_image_to_telegram.py
+++ publish_image_to_telegram.py
@@ -4,18 +4,6 @@ from dotenv import load_dotenv
 from time import sleep
 
 
-def send_message(directory, chat_id , publication_delay_time):
-    for file_name in os.listdir(directory):
-        with open(f"{directory}/{file_name}", 'rb') as photo:
-            bot.send_photo(chat_id=chat_id, photo=photo)
-        sleep(publication_delay_time)
-
-
-def send_endlessly_photos(directory, chat_id , publication_delay_time):
-    while True:
-        send_message(directory, chat_id , publication_delay_time)
-
-
 if __name__ == '__main__':
     load_dotenv()
     directory = "images"
@@ -24,4 +12,10 @@ if __name__ == '__main__':
     publication_delay_time = os.getenv("DELAY_TIME")
     publication_delay_time = int(publication_delay_time)
     bot = telegram.Bot(token=token)
-    send_endlessly_photos(directory, chat_id , publication_delay_time)
+    while True:
+        for file_name in os.listdir(directory):
+            with open(f"{directory}/{file_name}", 'rb') as photo:
+                bot.send_photo(chat_id=chat_id, photo=photo)
+            sleep(publication_delay_time)
+
+    
