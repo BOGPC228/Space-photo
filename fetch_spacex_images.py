78611a98ffe886751863e07906a09689d7e0e5e4
BOGPC228 <77198939+BOGPC228@users.noreply.github.com>
2022-07-21 11:52:49 +0300

    Rename and PEP8

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -10,12 +10,12 @@ def fetch_spacex_last_launch(directory):
     url = "https://api.spacexdata.com/v3/launches"
     response = requests.get(url)
     response.raise_for_status()
-    spacex_link = []
+    spacex_links = []
     all_spacex_starts = response.json()
     for start_one_spacex in all_spacex_starts:
         if start_one_spacex['links']['flickr_images']:
-            spacex_link+=start_one_spacex['links']['flickr_images']
-    for url_number, url in enumerate(spacex_link):
+            spacex_links += start_one_spacex['links']['flickr_images']
+    for url_number, url in enumerate(spacex_links):
         parse = urlparse(url)
         path, extension = os.path.splitext(parse.path)
         filename = f'spacex_{url_number}{extension}'
